2b71693ec065620a4493410ceb6dadd8695d4be9
Big Brother <aykibugz@yandex.ru>
2024-10-30 13:29:25 +0400

    test with form

---
+++ breaking_bad/static/scripts/formadd.js
@@ -0,0 +1,22 @@
+﻿function addRecord(form) {
+    let formData = new FormData(form);
+
+    for ( let val of formData.values() ) {
+        if ( !val ) { return; }
+    }
+
+    let table = document.getElementById("table");
+    let rows = length(table.rows);
+    let new_id = Number(document.getElementById("table").rows[rows].cells[0].textContent) + 1;
+    
+    
+
+
+    console.log( Object.fromEntries(formData) );
+    return Object.fromEntries(formData);
+}
+
+document.getElementById("addform").addEventListener("submit", function (e) {
+    e.preventDefault();
+    addRecord(e.target);
+});
