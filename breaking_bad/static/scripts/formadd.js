abf3980dda497f041807a9e60407f375b69cdf88
Big Brother <aykibugz@yandex.ru>
2024-10-30 14:12:37 +0400

    Ценок

--- breaking_bad/static/scripts/formadd.js
+++ breaking_bad/static/scripts/formadd.js
@@ -1,19 +1,19 @@
 ﻿function addRecord(form) {
     let formData = new FormData(form);
+    let vals = Object.values( Object.fromEntries(formData) );
 
-    for ( let val of formData.values() ) {
+    for ( let val of vals ) {
         if ( !val ) { return; }
     }
 
     let table = document.getElementById("table");
-    let rows = length(table.rows);
-    let new_id = Number(document.getElementById("table").rows[rows].cells[0].textContent) + 1;
     
-    
+    let row = table.insertRow(table.rows.length);
 
-
-    console.log( Object.fromEntries(formData) );
-    return Object.fromEntries(formData);
+    row.insertCell(0).innerHTML = table.rows.length;
+    for (let i = 0; i < 3; ++i) {
+        row.insertCell(i + 1).innerHTML = vals[i].replace(/[<>]/g, "").slice(0, 20);
+    }
 }
 
 document.getElementById("addform").addEventListener("submit", function (e) {
