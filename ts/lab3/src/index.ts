0f61cdf460a9ab1da8eb3df97b242aa9f91eadab
Big Brother <aykibugz@yandex.ru>
2024-11-27 15:53:58 +0400

    Create interfaces

--- ts/lab3/src/index.ts
+++ ts/lab3/src/index.ts
@@ -1,1 +1,26 @@
-﻿console.log(1);
+﻿enum IDTypes {
+    PASSPORT,
+    DRIVER_LICENSE,
+    BC
+}
+
+interface Owner {
+    surname: string;
+    name: string;
+    patronymic: string;
+    birthday: Date;
+    ID: IDTypes;
+    ser: number;
+    num: number;
+    printInfo(): void;
+}
+
+interface Vehicle {
+    make: string;
+    model: string;
+    yearIssue: Date;
+    VIN: string;
+    regNum: string;
+    owner: Owner;
+    printInfo(): void;
+}
