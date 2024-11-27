2af47aa0aae0acfa2578bcd7c9c06583796ba575
Big Brother <aykibugz@yandex.ru>
2024-11-27 13:09:45 +0400

    Init and create index.ts

---
+++ ts/lab1/dist/index.js
@@ -0,0 +1,19 @@
+const sum = (a, b, c) => {
+    return a + b + c;
+};
+const bool = true;
+let num = 999;
+const bi = BigInt(100);
+let str = "text";
+const some = 111;
+let func = sum;
+const id = 1;
+let obj = { name: "Alex", age: 25 };
+const data = {
+    id: 3,
+    e1: 999,
+    e2: "field"
+};
+const jsonString = JSON.stringify(data);
+console.log(jsonString);
+console.log(sum(1, 2, 3));
---
+++ ts/lab1/src/index.ts
@@ -0,0 +1,35 @@
+﻿const sum = (a: number, b: number, c: number): number => {
+    return a + b + c;
+}
+
+const bool: boolean = true;
+let num: number = 999;
+const bi: bigint = BigInt(100);
+let str: string = "text";
+const some: any = 111;
+let func: (a: number, b: number, c: number) => number = sum;
+const id: number | string = 1;
+let obj: {name: string, age?: number} = {name: "Alex", age: 25};
+
+interface Entity {
+    id: number;
+}
+
+interface toJsonStringify extends Entity {
+    e1?: number;
+    e2?: string;
+}
+
+const data: toJsonStringify = {
+    id: 3,
+    e1: 999,
+    e2: "field"
+}
+
+const jsonString: string = JSON.stringify(data);
+
+
+console.log(jsonString);
+
+
+console.log(sum(1,2,3));
