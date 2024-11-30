958dfb49f4ec25c1ee9c5d48235434c9ed55a55d
Big Brother <aykibugz@yandex.ru>
2024-11-30 14:21:18 +0400

    4 lab

---
+++ ts/lab3-4/dist/enums.d.ts
@@ -0,0 +1,32 @@
+export declare namespace Enums {
+    enum IDTypes {
+        PASSPORT = "Passport",
+        DRIVER_LICENSE = "Driver license",
+        BC = "Birthday Certificate"
+    }
+    enum bodyTypes {
+        MICRO = "Micro",
+        SEDAN = "Sedan",
+        CUV = "Crossover utility vehicle",
+        SUV = "Sport utility vehicle",
+        HATCHBACK = "Hatchback",
+        MINIVAN = "Minivan",
+        CABRIOLET = "Cabriolet",
+        COUPE = "Coupe",
+        ROADSTER = "Roadster",
+        SUPERCAR = "Supercar",
+        PICKUP = "Pickup",
+        VAN = "Van",
+        LIMOUSINE = "Limousine",
+        CAMPERVAN = "Campervan",
+        TRUCK = "Truck"
+    }
+    enum carClasses {
+        A = 0,
+        B = 1,
+        C = 2,
+        D = 3,
+        E = 4,
+        F = 5
+    }
+}
---
+++ ts/lab3-4/dist/owner.js
@@ -0,0 +1,80 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Owner {
+    constructor(surname, name, patronymic, birthday, ID, ser, num) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.birthday = birthday;
+        this.ID = ID;
+        this.ser = ser;
+        this.num = num;
+    }
+    get surname() {
+        return this._surname;
+    }
+    set surname(surname) {
+        this._surname = surname;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    get patronymic() {
+        return this._patronymic;
+    }
+    set patronymic(patronymic) {
+        this._patronymic = patronymic;
+    }
+    get birthday() {
+        return this._birthday;
+    }
+    set birthday(birthday) {
+        this._birthday = birthday;
+    }
+    get ID() {
+        return this._ID;
+    }
+    set ID(ID) {
+        this._ID = ID;
+    }
+    get ser() {
+        return this._ser;
+    }
+    set ser(ser) {
+        if (ser < 100000) {
+            this._ser = 0;
+            return;
+        }
+        if (ser % 1 !== 0) {
+            ser = Math.floor(ser);
+        }
+        this._ser = ser;
+    }
+    get num() {
+        return this._num;
+    }
+    set num(num) {
+        if (num < 1000) {
+            this._num = 0;
+            return;
+        }
+        if (num % 1 !== 0) {
+            num = Math.floor(num);
+        }
+        this._num = num;
+    }
+    printInfo() {
+        console.log(`
+            Surname: ${this.surname}
+            Name: ${this.name}
+            Patronymic: ${this.patronymic}
+            Birthday: ${this.birthday.toLocaleDateString("en-US")}
+            ID: ${this.ID}
+            Series: ${this.ser}
+            Number: ${this.num}
+            `);
+    }
+}
