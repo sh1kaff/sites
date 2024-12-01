b167c73c0ce942c886318b0e924d77a136cd0ecb
Big Brother <aykibugz@yandex.ru>
2024-12-01 19:17:54 +0400

    delete unused

--- ts/lab3/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare let owner: Owner;
-declare let vehicle: Vehicle;
-declare let car: Car;
-declare let car_2: Car;
-declare let car_3: Car;
-declare let motorbike: Motorbike;
-declare let vehicleStorage: VehicleStorage<Car>;
