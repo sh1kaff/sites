9f5e6cc465639877bb859910f7984cc32eb95bc2
Big Brother <aykibugz@yandex.ru>
2024-12-01 17:08:12 +0400

    Lab6

---
+++ ts/lab3-4-6/src/vehiclestorage.ts
@@ -0,0 +1,37 @@
+﻿import { Transport } from "./vehicle.js";
+
+interface IVehicleStorage<T extends Transport.Vehicle> {
+    creationDate: Date;
+    data: T[];
+    getData(): T[];
+}
+
+export class VehicleStorage<T extends Transport.Vehicle> implements IVehicleStorage<T> {
+    private _creationDate: Date;
+    private _data: T[];
+    
+    constructor(data: T[]) {
+        this.creationDate = new Date();
+        this.data = data;
+    }
+
+    get creationDate() {
+        return this._creationDate;
+    }
+
+    set creationDate(creationDate: Date) {
+        this._creationDate = creationDate;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    set data(data: T[]) {
+        this._data = data;
+    }
+    
+    getData(): T[] {
+        return this.data;
+    }
+}
